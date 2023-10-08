e5db34fa1d4c272dad7e5511c0e773cea5149e90
Hannah Kim <devhkim13@gmail.com>
2023-10-08 15:55:10 -0700

    Create a recursive solution to mergesort

---
+++ functions/mergesort.js
@@ -0,0 +1,11 @@
+const mergesort = (arr) => {
+  if (arr.length === 1) {
+    return arr;
+  }
+  
+  arr.sort((a, b) => a - b);
+  const left = mergesort(arr.slice(0, arr.length / 2));
+  const right = mergesort(arr.slice(arr.length / 2));
+
+  return left.concat(right);
+};
